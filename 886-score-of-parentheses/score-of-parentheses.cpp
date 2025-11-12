418577719cd6b824d202ca057ddf17374a713eba
maitobesthu <m00758942@gmail.com>
2025-11-12 12:37:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (19.62%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.cpp
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -3,6 +3,26 @@ class Solution
     public:
         int scoreOfParentheses(string s)
         {
+            //with stack
+            // stack<int> st;
+            // st.push(0);
+            // for (char ch: s)
+            // {
+            //     if (ch == '(')
+            //     {
+            //         st.push(0);
+            //     }
+            //     else
+            //     {
+            //         int v = st.top();
+            //         st.pop();
+            //         int add = max(v*2 , 1);
+            //         st.top() += add;
+            //     }
+            // }
+            // return st.top();
+
+            //without stack
             int depth = 0;
             int score = 0;
             for (int i = 0; i < s.size(); i++)
