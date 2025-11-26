25fb74b6979a7f67fc245eefd79a49525c2c49c9
maitobesthu <m00758942@gmail.com>
2025-11-26 18:37:34 +0530

    Time: 122 ms (92.42%) | Memory: 70.2 MB (93.13%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -25,35 +25,81 @@ k,vector<vector<vector<int>>>&dp){ if(i>=n || j>=m){ return 0;
     }
 };
 */
+// class Solution {
+// public:
+//     int numberOfPaths(vector<vector<int>>& grid, int k) {
+//         int n = grid.size(), m = grid[0].size();
+//         const int mod = 1e9 + 7;
+//         vector<vector<vector<int>>> dp(
+//             n, vector<vector<int>>(m, vector<int>(k, 0)));
+//         int startrem = grid[0][0] % k;
+//         dp[0][0][startrem] = 1;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (i == 0 && j == 0) {
+//                     continue;
+//                 }
+//                 for (int r = 0; r < k; r++) {
+//                     long long ways = 0;
+//                     if (i > 0) {
+//                         ways += dp[i - 1][j][r];
+//                     }
+//                     if (j > 0) {
+//                         ways += dp[i][j - 1][r];
+//                     }
+//                     if (ways == 0)
+//                         continue;
+//                     int newrem = (r + grid[i][j]) % k;
+//                     dp[i][j][newrem] = (dp[i][j][newrem] + ways) % mod;
+//                 }
+//             }
+//         }
+//         return dp[n - 1][m - 1][0];
+//     }
+// };
+
 class Solution {
 public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int n = grid.size(), m = grid[0].size();
         const int mod = 1e9 + 7;
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(k, 0)));
+        vector<vector<int>> prev(m, vector<int>(k, 0));
+        vector<vector<int>> curr(m, vector<int>(k, 0));
         int startrem = grid[0][0] % k;
-        dp[0][0][startrem] = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
+        prev[0][startrem] = 1;
+        for (int j = 1; j < m; j++) {
+            for (int r = 0; r < k; r++) {
+                if (prev[j - 1][r]) {
+                    int newrem = (r + grid[0][j]) % k;
+                    prev[j][newrem] = (prev[j][newrem] + prev[j - 1][r]) % mod;
                 }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++){
+                for (int r = 0; r < k; r++){
+                    curr[j][r] = 0;
+                }
+            }
+            for (int j = 0; j < m; j++) {
                 for (int r = 0; r < k; r++) {
                     long long ways = 0;
                     if (i > 0) {
-                        ways += dp[i - 1][j][r];
+                        ways += prev[j][r];
                     }
                     if (j > 0) {
-                        ways += dp[i][j - 1][r];
+                        ways += curr[j - 1][r];
                     }
                     if (ways == 0)
                         continue;
                     int newrem = (r + grid[i][j]) % k;
-                    dp[i][j][newrem] = (dp[i][j][newrem] + ways) % mod;
+                    curr[j][newrem] = (curr[j][newrem] + ways) % mod;
                 }
+                
             }
+            prev=curr;
         }
-        return dp[n - 1][m - 1][0];
+        return prev[m - 1][0];
     }
 };
